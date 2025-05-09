aacd0a5664f8b1cd904dca5c478b7c751f5e2987
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-09 04:32:45 +0200

    Connect to database on server start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
 import startServer from "./server/startServer.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const port = process.env.PORT ?? 3001;
 
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const databaseUri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster-pro-max.zzkvldx.mongodb.net/insectopedia`;
+
+await connectToDatabase(databaseUri);
 startServer(port);

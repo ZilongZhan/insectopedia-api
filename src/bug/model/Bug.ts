94e161b336c2626fcceb20e5074bfa7a5147ca19
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-16 18:44:11 +0200

    Add schema validation to BugModel

--- src/bug/model/Bug.ts
+++ src/bug/model/Bug.ts
@@ -4,38 +4,50 @@ import { BugStructure } from "../types.js";
 const BugSchema = new Schema<BugStructure>({
   commonName: {
     type: String,
+    minlength: [3, "Minimum 3 characters required"],
+    maxlength: [50, "Maximum 50 characters allowed"],
     required: true,
   },
   latinName: {
     type: String,
-    required: true,
-  },
-  class: {
-    type: String,
+    minlength: [3, "Minimum 3 characters required"],
+    maxlength: [50, "Maximum 50 characters allowed"],
     required: true,
   },
   description: {
     type: String,
+    minlength: [50, "Minimum 50 characters required"],
+    maxlength: [400, "Maximum 400 characters allowed"],
     required: true,
   },
   imageUrl: {
     type: String,
+    match: [/\.(jpg|jpeg|png|gif|webp)$/i, "Invalid media/url format"],
     required: true,
   },
   isDangerous: {
     type: Boolean,
+    default: false,
     required: true,
   },
   isFavorite: {
     type: Boolean,
-    required: true,
-  },
-  order: {
-    type: String,
+    default: false,
     required: true,
   },
   phylum: {
     type: String,
+    default: "other",
+    required: true,
+  },
+  class: {
+    type: String,
+    default: "other",
+    required: true,
+  },
+  order: {
+    type: String,
+    default: "other",
     required: true,
   },
 });

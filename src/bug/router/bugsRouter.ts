aedf6e267c4f2355f31e6a5d1921e5546e5d02fa
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-21 16:41:40 +0200

    Add DELETE method endpoint to bugsRouter

--- src/bug/router/bugsRouter.ts
+++ src/bug/router/bugsRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import BugsController from "../controller/BugsController.js";
 import Bug from "../model/Bug.js";
+import handleValidateId from "../../server/middlewares/handleValidateId/handleValidateId.js";
 
 const bugsRouter = Router();
 
@@ -9,5 +10,7 @@ const bugsController = new BugsController(Bug);
 bugsRouter.get("/", bugsController.getBugsData);
 
 bugsRouter.post("/", bugsController.addBug);
+
+bugsRouter.delete("/:id", handleValidateId, bugsController.deleteBugById);
 
 export default bugsRouter;
